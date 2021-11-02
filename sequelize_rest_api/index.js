0432a129d2784da3bde97e0a474c7e72831e8086
Ross Keeley <ross.keeley@sky.uk>
2021-11-02 23:28:43 +0000

    created endpoint to POST new menu

--- sequelize_rest_api/index.js
+++ sequelize_rest_api/index.js
@@ -80,7 +80,87 @@ app.put('/api/restaurants/:id', async (req, res) => {
         res.status(400).send(e.message);
     }
 });
+
+
+
+
+
+
+
 // 4. create a suite of menu and menu item routes that will CRUD each resource
+app.post('/api/menus', async (req, res) => {
+    try {
+    // create a row in the database using sequelize create method
+    const menu = await Menu.create(req.body);
+    
+    // 201 = created a resource
+    res.status(201).send(menu);
+    } catch (e) {
+    res.status(400).send(e.message);
+    }
+})
+
+// app.get('/api/restaurants', async (req, res) => {
+//     try {
+//     // create a row in the database using sequelize create method
+//     const restaurants = await Restaurant.findAll({});
+
+//     // 200 = success
+//     res.status(200).send(restaurants);
+//     } catch (e) {
+//     res.status(400).send(e.message);
+//     }
+// });
+  
+// // 1. create an endpoint that will get a restaurant by ID (HTTP Method = get)
+// app.get('/api/restaurants/:id', async (req, res) => {
+//     try {
+//         const restaurants = await Restaurant.findAll({
+//             where: req.params
+//         });
+
+//         // 200 = success
+//         res.status(200).send(restaurants);
+//     } catch (e) {
+//         res.status(400).send(e.message);
+//     }
+// });
+
+// // 2. create an endpoint that will delete a restaurant by ID (HTTP Method = delete)
+// app.delete('/api/restaurants/:id', async (req, res) => {
+//     try {
+//         const restaurants = await Restaurant.destroy({
+//             where: req.params
+//         });
+
+//         // 200 = success
+//         res.status(200).send(restaurants);
+//     } catch (e) {
+//         res.status(400).send(e.message);
+//     }
+// });
+// // 3. create an endpoint that will update a restaurant by ID (HTTP Method = put)
+// app.put('/api/restaurants/:id', async (req, res) => {
+//     try {
+//         const restaurant = await Restaurant.update(req.body, {
+//             where: req.params
+//         });
+
+//         const restaurantUpdated = await Restaurant.findAll({
+//             where: req.params
+//         }); 
+
+//         // 200 = success
+//         res.status(200).send(restaurantUpdated);
+//     } catch (e) {
+//         res.status(400).send(e.message);
+//     }
+// });
+
+
+
+
+
 
 // 5. find a way to relate the menu items to the menu and the menu to the restaurant
 
